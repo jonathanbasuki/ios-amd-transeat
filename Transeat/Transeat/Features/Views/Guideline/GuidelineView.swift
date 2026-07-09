cb91d7451991856907542f1635c05301a9315ee6
Naila <naila.lauza02@icloud.com>
2026-07-09 09:12:11 +0700

    feat: add tnc, upload status, ocr, guideline

---
+++ Transeat/Transeat/Features/Views/Guideline/GuidelineView.swift
@@ -0,0 +1,15 @@
+//
+//  Guideline.swift
+//  Transeat
+//
+//  Created by Naila Lauza on 09/07/26.
+//
+
+
+Image(systemName: "questionmark.circle")
+                .font(.system(size: 20))
+                .foregroundColor(Color.hexPalette.cherry500)
+                
+            Image(systemName: "person.circle.fill")
+                .font(.system(size: 32))
+                .foregroundColor(Color.hexPalette.cherry500)
